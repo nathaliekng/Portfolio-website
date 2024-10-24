771bbfb55fac5911c264bdd7da8c9e0da84f5d3d
Nathalie Ng <41026746+nathaliekng@users.noreply.github.com>
2024-10-24 18:27:20 -0400

    update project card

---
+++ app/components/ExperienceSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ExperienceSection = () => {
+  return (
+    <section id="experience">
+        <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        Experience
+      </h2>
+
+    </section>
+  )
+}
+
+export default ExperienceSection
