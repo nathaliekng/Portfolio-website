6d03b3d18efb3561add98ff2db24c983012198fa
Nathalie Ng <41026746+nathaliekng@users.noreply.github.com>
2024-10-24 19:01:40 -0400

    updated photos

--- app/components/DownloadButton.jsx
+++ app/components/DownloadButton.jsx
@@ -2,7 +2,7 @@ const DownloadButton = ({ file, downloadName }) => {
   return (
     <a
       href={file}
-      className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+      className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-primary-900 hover:bg-slate-800 text-white mt-3"
       download={downloadName}
     >
       <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -72,7 +72,8 @@ const HeroSection = () => {
             <Image
               src="/images/hero-image.png"
               alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              // className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              className="rounded-full w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] object-cover"
               width={300}
               height={300}
             />
