46fb1ec3dd85dd39ea91e6d3cf565b569ba401db
Nathalie Ng <41026746+nathaliekng@users.noreply.github.com>
2024-12-01 17:27:13 -0500

    mobile view update

--- app/components/ExperienceSection.jsx
+++ app/components/ExperienceSection.jsx
@@ -31,7 +31,7 @@ const ExperienceSection = () => {
       </h2>
       <div>
         {experiences.map((experience, index) => (
-          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+          <div key={index} className="mb-8 flex flex-wrap lg:justify-center sm:justify-center">
             <motion.div 
             whileInView={{opacity:1, x:0}}
             initial={{opacity:0, x:-100}}
