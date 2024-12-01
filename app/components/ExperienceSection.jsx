c032396c5bae5ad70f546f29f9ebfd65fbc8c030
Nathalie Ng <41026746+nathaliekng@users.noreply.github.com>
2024-12-01 17:45:18 -0500

    remove animation

--- app/components/ExperienceSection.jsx
+++ app/components/ExperienceSection.jsx
@@ -31,8 +31,8 @@ const ExperienceSection = () => {
       </h2>
       <div>
         {experiences.map((experience, index) => (
-          <div key={index} className="mb-8 flex flex-wrap lg:justify-center sm:justify-center">
-            <motion.div 
+          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+            {/* <motion.div 
             whileInView={{opacity:1, x:0}}
             initial={{opacity:0, x:-100}}
             transition={{duration:1}}
@@ -60,7 +60,31 @@ const ExperienceSection = () => {
                     <span key={index} className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800">{tech}</span>
                   ))
                 }
-            </motion.div>
+            </motion.div> */}
+            <div
+            className="w-full lg:w-1/4">
+              <p className="mb-2 text-sm text-neutral-400">{`${experience.startDate} - ${experience.endDate}`}</p>
+            </div>
+            <div 
+
+            className="w-full max-w-vl lg:w-3/4">
+                <h6 className="mb-2 font-semibold">
+                  {experience.role} - <span className="text-sm text-purple-100">
+                    {experience.company}
+                  </span>
+                </h6>
+                <ul className="list-disc ">
+                  {experience.description.map((exp, index) => (
+                    <li key={index} className="ml-8 mb-4 text-neutral-400">{exp}</li>
+                  )
+                  )}
+                </ul>
+                {
+                  experience.technologies.map((tech, index) =>(
+                    <span key={index} className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800">{tech}</span>
+                  ))
+                }
+            </div>
           </div>
         ))}
       </div>
