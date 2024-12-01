d272c17356217e7afd550cc053ed32fbc4d44322
Nathalie Ng <41026746+nathaliekng@users.noreply.github.com>
2024-12-01 17:51:39 -0500

    fixed animation mobile view

--- app/components/ExperienceSection.jsx
+++ app/components/ExperienceSection.jsx
@@ -31,8 +31,8 @@ const ExperienceSection = () => {
       </h2>
       <div>
         {experiences.map((experience, index) => (
-          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-            {/* <motion.div 
+          <div key={index} className="mb-8 flex flex-wrap lg:justify-center overflow-x-hidden" >
+            <motion.div 
             whileInView={{opacity:1, x:0}}
             initial={{opacity:0, x:-100}}
             transition={{duration:1}}
@@ -60,8 +60,8 @@ const ExperienceSection = () => {
                     <span key={index} className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800">{tech}</span>
                   ))
                 }
-            </motion.div> */}
-            <div
+            </motion.div>
+            {/* <div
             className="w-full lg:w-1/4">
               <p className="mb-2 text-sm text-neutral-400">{`${experience.startDate} - ${experience.endDate}`}</p>
             </div>
@@ -84,7 +84,7 @@ const ExperienceSection = () => {
                     <span key={index} className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800">{tech}</span>
                   ))
                 }
-            </div>
+            </div> */}
           </div>
         ))}
       </div>
